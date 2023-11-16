836b78f81bf13c63a86e76e708b50d2627310ae2
hhimanshii108 <100212269+hhimanshii108@users.noreply.github.com>
2023-11-16 14:34:03 +0000

    Data Types in javascript

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS  code as newer version
+// alert(3+3) // we are using node js not browser
+
+//Primitive data types
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => no value assigned
+// symbol => to find uniqueness in components
+
+//object
+
+console.log(typeof "hima")
+console.log(typeof 3)
+console.log(typeof null)
+console.log(typeof undefined)
+
+// typeof null is object and typeof undefined is undefined
+
+// console.table(typeof "hima",typeof 3,typeof null,typeof undefined)
