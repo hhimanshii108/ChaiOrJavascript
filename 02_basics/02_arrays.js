1e58f8abeb4709091e40f6d1601bd2bf22ea007a
hhimanshii108 <100212269+hhimanshii108@users.noreply.github.com>
2023-11-18 10:17:05 +0000

    part 2 arrays js

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,28 @@
+const fname=["hima","nikhil","yagya"]
+const lname=["rana","kotiyal","kulora"]
+
+// fname.push(lname)
+// console.log(fname);
+// pushes the array as an element in fname
+
+const name=fname.concat(lname)
+//console.log(name);
+// concat pushes all elements as different element sin array
+
+const names2=[...fname,...lname]
+//console.log(names2);
+
+const mixture=[1,2,3,[5,6,[1,9,[0]]],34,56,90,[100]]
+const flatArray=mixture.flat(Infinity)
+console.log(flatArray);
+
+// isArray
+console.log(Array.isArray("hima"));
+console.log(Array.from("himanshi"));
+//console.log(Array.from({name:"hima"}));  we have to tell key or value
+
+let score1=100
+let score2=200
+let score3=300
+
+console.log(Array.of(score1,score2,score3));
