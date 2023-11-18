0084451865ac4561760da2c4b2d46fa350cf5d9f
hhimanshii108 <100212269+hhimanshii108@users.noreply.github.com>
2023-11-18 11:50:25 +0000

    detail discussion on funtions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -30,5 +30,41 @@ function loginUserMessage(username="nk")// can also give default values
     }
     return `${username} just logged in`
 }
-console.log(loginUserMessage("hima"));
+//console.log(loginUserMessage("hima"));
 // if no value passed then it returns undefined
+
+// shopping cart situation , if user adds n items , then total
+
+function calculateCartPrice(val1,val2,...num1)// these operators are called rest here
+{
+    return num1
+}
+console.log(calculateCartPrice(200,400,500))
+
+//passing objects in functions
+
+const user={
+    username:"hima",
+    price:20
+}
+
+function handleObject(obj)
+{
+    console.log(`Username is ${obj.username} and price is ${user.price}`)
+}
+handleObject(user)
+handleObject({
+    username:"nk",
+    price:990
+})
+
+// passing arrays in functions
+
+const dummyArray=[100,200,300,400]
+
+function returnSecondVal(getArray)
+{
+    return getArray[1]
+}
+console.log(returnSecondVal(dummyArray))
+console.log(returnSecondVal([1,2,3,4,5]))
