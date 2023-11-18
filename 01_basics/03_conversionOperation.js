f6b411307632e8665e0ee4325c189224bc4c1d55
hhimanshii108 <100212269+hhimanshii108@users.noreply.github.com>
2023-11-18 05:43:34 +0000

    Conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+// converting string in Number
+// "33" => 33
+// "33abc" => NaN
+// true =>1, false => 0
+//
+  
+// converting number in Boolean
+// 1=> true
+// 0 => false
+// "" => false
+// "hima" => true
+
+let score=undefined
+console.log(score);
+let num=Number(score);
+console.log(typeof score);
+console.log(typeof num);
+console.log(num);
