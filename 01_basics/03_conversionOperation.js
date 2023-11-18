88ba1ef4588e6e65ac3b1c5798f7017dd053bdd3
hhimanshii108 <100212269+hhimanshii108@users.noreply.github.com>
2023-11-18 05:59:27 +0000

    operations in js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -11,8 +11,42 @@
 // "hima" => true
 
 let score=undefined
-console.log(score);
+// console.log(score);
 let num=Number(score);
-console.log(typeof score);
-console.log(typeof num);
-console.log(num);
+// console.log(typeof score);
+// console.log(typeof num);
+// console.log(num);
+
+
+//*********************Operations *********** */
+
+// Basic operations
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1="hello"
+let str2=" hima"
+let str3=str1+str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+3);
+// console.log(1+2+"3");
+
+// if the type of first element is tring then all the others are treated as string
+// else numbers
+
+console.log(true);
+console.log(+true); // + converts it to Number
+console.log(+"");  // Number of empty string is false
+
+// console.log(true+) // error
+
+let num1,num2,num3
+num1=num2=num3=2+2
