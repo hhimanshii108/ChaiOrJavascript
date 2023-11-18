5a520799a9fccbc71f01cd07d958be7fc6314860
hhimanshii108 <100212269+hhimanshii108@users.noreply.github.com>
2023-11-18 08:04:04 +0000

    numbers and math in js

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,32 @@
+const score = 300
+// console.log(score);
+const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const num=23.5786
+// console.log(num.toPrecision(2));
+
+const hundreds=100000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+
+//++++++++++++++++++++++ Math ++++++++++
+
+
+//console.log(Math);
+// console.log(Math.abs(-34));
+// console.log(Math.round(3.56));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(2.3));
+
+console.log(Math.random()); // values always between 0 and 1
+console.log((Math.random()*10)+1);
+
+const min=10
+const max=20;
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+// random gives values between 0 and 1
+
